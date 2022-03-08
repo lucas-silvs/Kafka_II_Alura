73ef8fe6bf4e50cfbec021ed3a5e3acf63f0eea6
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-03-08 17:33:01 -0300

    Kafka II Alura - finalização do modulo 3: - uso de um servlet(servidor http embarcado) que ao receber requisição get envia um pedido com email para os topicos existentes - discutido a vantagem do fast delegate, que é basicamente priorizar menos código no servlet afim de evitar que bugs ocorram  no processo para postar uma mensagem

---
+++ kafka-I-Alura-Java-Project/service--http-eccomerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -0,0 +1,62 @@
+package br.com.alura.ecommerce;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderServlet extends HttpServlet{
+    private final KafkaDispatcher<Order> dispatcher = new KafkaDispatcher<>();
+    private final KafkaDispatcher<Email>  emailDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        dispatcher.close();
+        emailDispatcher.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try (dispatcher) {
+            try (emailDispatcher){
+                try {
+                    var valorreq = Integer.parseInt(req.getParameter("valor"));
+                    var email = req.getParameter("email");
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(valorreq);
+
+
+                    var order = new Order(orderId, amount, email);
+                    dispatcher.send("ECOMMERCE_NOVO_PEDIDO", email, order);
+
+
+                    var emailRequest = new Email("Teste@email.com","Obrigado, estamos processando seu pedido");
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailRequest);
+
+                    System.out.println("Processo da compra encerrado");
+                    resp.getWriter().println("Processo de compra encerrado");
+                    resp.setStatus(HttpServletResponse.SC_OK);
+
+
+                } catch (ExecutionException e) {
+                    throw new ServletException(e);
+                } catch (InterruptedException e) {
+                    throw new ServletException(e);
+                }
+            }
+        }
+
+    }
+}
